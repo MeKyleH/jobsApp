2471313d86a143f941487dfba7df2f555589bbab
mekyleh <kyleholt@Kyles-MacBook-Pro.local>
2018-01-14 19:55:37 -0800

    finishing up last of navigation

--- screens/ReviewScreen.js
+++ screens/ReviewScreen.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import { Button } from 'react-native-elements';
 
 class ReviewScreen extends Component {
+  static navigationOptions = ({ navigation }) => {
+    return {
+       title: 'Review Jobs',
+       headerRight: <Button title="Settings" onPress={() => navigation.navigate('settings')} />
+    };
+  };
+
   render() {
     return (
       <View>

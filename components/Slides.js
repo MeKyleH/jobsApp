e798a6215f90bdbe467089865dbc48e6e0997180
mekyleh <kyleholt@Kyles-MacBook-Pro.local>
2018-01-14 21:45:37 -0800

    adding continue button to last slide

--- components/Slides.js
+++ components/Slides.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
 import { Dimensions, ScrollView, Text, View } from 'react-native';
+import { Button } from 'react-native-elements';
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
 
 class Slides extends Component {
+  renderLastSlide(index) {
+    if (index === this.props.data.length - 1) {
+      return (
+        <Button
+          title="Get Started Now"
+          raised
+        />
+      );
+    }
+  }
+
   renderSlides() {
-    return this.props.data.map((slide) => {
+    return this.props.data.map((slide, index) => {
       return (
         <View
         key={slide.text}
@@ -13,6 +25,7 @@ class Slides extends Component {
           styles.slideStyle, { backgroundColor: slide.color }]}
         >
           <Text style={styles.textStyle}>{slide.text}</Text>
+          {this.renderLastSlide(index)}
         </View>
       );
     });

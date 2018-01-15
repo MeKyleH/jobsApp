28ae29ed17f3d66b307b505016d813ae129d9bcc
mekyleh <kyleholt@Kyles-MacBook-Pro.local>
2018-01-14 19:31:37 -0800

    adding all navigation screens

--- App.js
+++ App.js
@@ -4,12 +4,24 @@ import { TabNavigator } from 'react-navigation';
 
 import AuthScreen from './screens/AuthScreen';
 import WelcomeScreen from './screens/WelcomeScreen';
+import MapScreen from './screens/MapScreen';
+import DeckScreen from './screens/DeckScreen';
+import ReviewScreen from './screens/ReviewScreen';
+import SettingsScreen from './screens/SettingsScreen';
 
 export default class App extends React.Component {
   render() {
     const MainNavigator = TabNavigator({
       welcome: { screen: WelcomeScreen },
-      auth: { screen: AuthScreen }
+      auth: { screen: AuthScreen },
+      main: {
+        screen: TabNavigator({
+          map: { screen: MapScreen },
+          deck: { screen: DeckScreen },
+          review: { screen: ReviewScreen },
+          settings: { screen: SettingsScreen },
+        })
+      }
     });
 
     return (

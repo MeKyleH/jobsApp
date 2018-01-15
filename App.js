df8d3da0a1da650ca224b16bdc66b7ffaac9f696
mekyleh <kyleholt@Kyles-MacBook-Pro.local>
2018-01-14 19:25:54 -0800

    first commit

---
+++ App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { TabNavigator } from 'react-navigation';
+
+import AuthScreen from './screens/AuthScreen';
+import WelcomeScreen from './screens/WelcomeScreen';
+
+export default class App extends React.Component {
+  render() {
+    const MainNavigator = TabNavigator({
+      welcome: { screen: WelcomeScreen },
+      auth: { screen: AuthScreen }
+    });
+
+    return (
+      <View style={styles.container}>
+        <MainNavigator />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});

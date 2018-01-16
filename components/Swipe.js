e62040629f361c287b45fd4f2525b39d66afdde3
mekyleh <kyleholt@Kyles-MacBook-Pro.local>
2018-01-15 19:33:39 -0800

    fixing some styling bugs

---
+++ components/Swipe.js
@@ -0,0 +1,149 @@
+import React, { Component } from 'react';
+import {
+  Animated,
+  Dimensions,
+  LayoutAnimation,
+  PanResponder,
+  Platform,
+  UIManager,
+  View
+} from 'react-native';
+
+const SCREEN_WIDTH = Dimensions.get('window').width;
+const SWIPE_THRESHOLD = SCREEN_WIDTH * 0.25;
+const SWIPE_OUT_DURATION = 250; //ms
+
+/**
+  this.props.data data for the cards
+  this.props.renderNoMoreCards() what to do after done swiping
+  this.props.renderCard(item) how to render card/item
+  this.props.keyProp string label for unique key
+
+**/
+class Swipe extends Component {
+  static defaultProps = {
+    onSwipeRight: () => {},
+    onSwipeLeft: () => {},
+    keyProp: 'id'
+  }
+
+  constructor(props) {
+    super(props);
+
+    const position = new Animated.ValueXY();
+    const panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderMove: (event, gesture) => {
+        position.setValue({ x: gesture.dx, y: gesture.dy });
+      },
+      onPanResponderRelease: (event, gesture) => {
+        if (gesture.dx > SWIPE_THRESHOLD) {
+          this.forceSwipe(1);
+        } else if (gesture.dx < -SWIPE_THRESHOLD) {
+          this.forceSwipe(-1);
+        } else {
+          this.resetPosition();
+        }
+      }
+    });
+
+    this.position = position;
+    this.state = { panResponder, index: 0 };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.data !== this.props.data) {
+      this.setState({ index: 0 });
+    }
+  }
+
+  componentWillUpdate() {
+    // specifically for Android
+    UIManager.setLayoutAnimationEnabledExperimental &&
+      UIManager.setLayoutAnimationEnabledExperimental(true);
+    LayoutAnimation.spring();
+  }
+
+  getCardStyle() {
+    const rotate = this.position.x.interpolate({
+      inputRange: [-SCREEN_WIDTH, 0, SCREEN_WIDTH],
+      outputRange: ['-30deg', '0deg', '30deg']
+    });
+
+    return {
+      ...this.position.getLayout(),
+      transform: [{ rotate }]
+    };
+  }
+
+  forceSwipe(direction) {
+    Animated.timing(this.position, {
+      toValue: { x: 2 * SCREEN_WIDTH * direction, y: 0 },
+      duration: SWIPE_OUT_DURATION
+    }).start(() => this.completeSwipe(direction));
+  }
+
+  completeSwipe(direction) {
+    const { onSwipeLeft, onSwipeRight, data } = this.props;
+    const item = data[this.state.index];
+
+    direction === 0 ? onSwipeRight(item) : onSwipeLeft(item);
+    this.position.setValue({ x: 0, y: 0 });
+    this.setState({ index: this.state.index + 1 });
+  }
+
+  resetPosition() {
+    Animated.spring(this.position, {
+      toValue: { x: 0, y: 0 }
+    }).start();
+  }
+
+  renderCards() {
+    if (this.state.index >= this.props.data.length) {
+      return this.props.renderNoMoreCards();
+    }
+
+    const deck = this.props.data.map((item, i) => {
+      if (i < this.state.index) { return null; }
+
+      if (i === this.state.index) {
+        return (
+          <Animated.View
+            key={item[this.props.keyProp]}
+            style={[this.getCardStyle(), styles.cardStyle]}
+            {...this.state.panResponder.panHandlers}
+          >
+            {this.props.renderCard(item)}
+          </Animated.View>
+        );
+      }
+
+      return (
+        <Animated.View
+        key={item[this.props.keyProp]}
+          style={[styles.cardStyle, { top: 10 * (i - this.state.index), elevation: 4 }]}
+        >
+          {this.props.renderCard(item)}
+        </Animated.View>
+      );
+    });
+    return Platform.OS === 'android' ? deck : deck.reverse();
+  }
+
+  render() {
+    return (
+      <View>
+        {this.renderCards()}
+      </View>
+    );
+  }
+}
+
+const styles = {
+  cardStyle: {
+    position: 'absolute',
+    width: SCREEN_WIDTH
+  }
+};
+
+export default Swipe;
--- screens/DeckScreen.js
+++ screens/DeckScreen.js
@@ -1,18 +1,73 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import { Button, Card } from 'react-native-elements';
+import { connect } from 'react-redux';
+import { MapView } from 'expo';
+import Swipe from '../components/Swipe';
 
 class DeckScreen extends Component {
+  renderCard(job) {
+    const initialRegion = {
+      longitude: job.longitude,
+      latitude: job.latitude,
+      longitudeDelta: 0.02,
+      latitudeDelta: 0.045
+    };
+
+    return (
+      <Card title={job.jobtitle}>
+        <View style={{ height: 300 }}>
+          <MapView
+            scrollEnabled={false}
+            style={{ flex: 1 }}
+            cacheEnabled={true}
+            initialRegion={initialRegion}
+          >
+
+          </MapView>
+        </View>
+        <View style={styles.detailWrapper}>
+          <Text>{job.company}</Text>
+          <Text>{job.formattedRelativeTime}</Text>
+        </View>
+        <Text>
+          {job.snippet.replace(/<b>/g, '').replace(/<\/b/g, '')}
+        </Text>
+      </Card>
+    );
+  }
+
+  renderNoMoreCards() {
+    return (
+      <Card title="No more jobs">
+      </Card>
+    );
+  }
+
   render() {
     return (
       <View>
-        <Text>DeckScreen</Text>
-        <Text>DeckScreen</Text>
-        <Text>DeckScreen</Text>
-        <Text>DeckScreen</Text>
-        <Text>DeckScreen</Text>
+        <Swipe
+          data={this.props.jobs}
+          renderNoMoreCards={this.renderNoMoreCards}
+          renderCard={this.renderCard}
+          keyProp="jobkey"
+        />
       </View>
     );
   }
 }
 
-export default DeckScreen;
+const styles = {
+  detailWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginBottom: 10
+  }
+};
+
+const mapStateToProps = ({ jobs }) => {
+  return { jobs: jobs.results };
+};
+
+export default connect(mapStateToProps)(DeckScreen);
